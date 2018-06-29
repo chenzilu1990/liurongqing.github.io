c3b7468b05b55d74493956fa71edae2b71dab0c2
liurongqing <phpgege@163.com>
2018-06-29 09:21:10 +0800

    修复滚动条

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,17 @@
 (function () {
 
-    window.onload = function(){
+    window.onload = function () {
         let progress = document.createElement('div');
         progress.className = 'progress';
         document.body.appendChild(progress);
 
         let de = document.documentElement;
 
-        alert('scrollheight: '+de.scrollHeight)
-        alert('clientHeight: '+de.clientHeight)
-        
-
-        let scrollHeight = de.scrollHeight  - de.clientHeight;
+        let scrollHeight = de.scrollHeight - de.clientHeight;
         let scroll = 0;
-        window.onscroll = function(){
-            // scroll = 
-            progress.innerHTML = 'scrollTop: '+document.documentElement.scrollTop + 'body: '+document.body.scrollTop;
-            progress.style.width = (de.scrollTop / scrollHeight) * 100 + '%';
+        window.onscroll = function () {
+            scroll = de.scrollTop || document.body.scrollTop;
+            progress.style.width = (scroll / scrollHeight) * 100 + '%';
         }
 
     }
