c1fbc285329857d317d2be9a5b57971f0f57ce3d
liurongqing <phpgege@163.com>
2018-06-29 09:02:33 +0800

    调试手机进度条问题

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,14 @@
 
         let de = document.documentElement;
 
+        alert('scrollheight: '+de.scrollHeight)
+        alert('clientHeight: '+de.clientHeight)
+        
+
         let scrollHeight = de.scrollHeight  - de.clientHeight;
         window.onscroll = function(){
+            progress.innerHTML = 'scrollTop: '+de.scrollTop;
+            document.querySelector('.footer').innerHTML = 'scrollTop: '+de.scrollTop;
             progress.style.width = (de.scrollTop / scrollHeight) * 100 + '%';
         }
 
