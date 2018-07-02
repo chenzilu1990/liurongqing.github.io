7428e2ad4b79ae10be0d4787ad48375c2ed4c915
liurongqing <phpgege@163.com>
2018-07-02 18:04:51 +0800

    添加service-worker缓存

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+
+let CACHE_NAME = 'liurongqing-v2';
+let urlsToCache = [
+    '/assets/css/style.css',
+    '/assets/js/main.js',
+    '/assets/images/favicon.ico',
+    '/assets/images/logo.svg',
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
