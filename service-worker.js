4b14c6b7a682cd8f8a8d29c23a8b6d7e7e9da4b8
liurongqing <phpgege@163.com>
2018-07-03 09:52:31 +0800

    添加页面缓存

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 
-let CACHE_NAME = 'liurongqing-v2018-07-02-09';
+let CACHE_NAME = 'liurongqing-v2018-07-02-09-52';
 
 // 页面文件
 let pageToCache = ["/mathjax-latex", "/high-performance", "/nginx-gzip", "/yahoo-35", "/phaser2-custom", "/linux-crontab", "/css-rect", "/gitignore", "/server-git", "/shortcuts", "/vscode", "/https-encrypt", "/gulp", "/git-use", "/linux-all", "/nvm", "/gogs", "/scss", "/ghost", "/mnmp", "/mac-software", "/jekyll-github", "/react-1", "/react-2", "/phaser-2048-1", "/phaser-2048-2", "/phaser-2048-3", "/phaser-2048-4", "/phaser-2048-5", "/phaser-2048-6"];
 
 let resToCache = [
+    '/',
     '/assets/css/style.css',
     '/assets/js/main.js',
     '/assets/images/favicon.ico',
     '/assets/images/logo.svg',
-    '/',
 ];
 
 let cache_list = pageToCache.concat(resToCache);
@@ -33,7 +33,7 @@ self.addEventListener('activate', function (event) {
             .then(function (keys) {
                 return Promise.all(keys
                     .filter(function (key) {
-                            return key !== CACHE_NAME;
+                        return key !== CACHE_NAME;
                     })
                     .map(function (key) {
                         return caches.delete(key);
