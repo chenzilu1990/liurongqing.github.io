f65d94f859ea4b7e3b354fbf470c6896bfd4b3e0
liurongqing <phpgege@163.com>
2018-07-20 11:39:49 +0800

    添加标签路径到缓存中

--- pages.js
+++ pages.js
@@ -2,20 +2,36 @@ const fs = require('fs')
 
 let exp = /\d{4}-\d{2}-\d{2}-/i;
 let arr = [];
+let allFiles = [];
 
-readFile('./_posts')
+readFile('./tag',function(data){
+    readFile('./_posts',function(data2){
+        allFiles = data.concat(data2);
+        writeFile(JSON.stringify(allFiles));
+    })
+})
 
-function readFile(url) {
+function readFile(url, cb) {
     fs.readdir(url, function (err, files) {
         if (err) {
             console.error(err)
         } else {
-            files.forEach(function (filename) {
-                if (exp.test(filename)) {
-                    arr.push(filename.replace(exp, '/').replace('.md', ''));
-                }
-            });
-            writeFile(JSON.stringify(arr));
+            if (url.search('tag') > 0) {
+                files.forEach(function (filename) {
+
+                    // const isDir = fs.statSync(filename).isDirectory();
+                    arr.push('/tag/' + filename + '/');
+                });
+            } else {
+                files.forEach(function (filename) {
+
+                    if (exp.test(filename)) {
+                        arr.push(filename.replace(exp, '/').replace('.md', ''));
+                    }
+                });
+            }
+
+            cb && cb(arr);
         }
     })
 }
